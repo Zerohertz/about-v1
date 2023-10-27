e5aa10ce1b76e0355f32533aeff3db90c5547285
Zerohertz <ohg3417@gmail.com>
2023-10-27 20:29:07 +0900

    :art: Config: Themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,5 +12,42 @@ export default function YosumeApp({
   pageProps: any;
 }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        .badge-primary,
+        .btn-primary,
+        .bg-primary {
+          background-color: #800a0a !important;
+          border-color: #800a0a !important;
+        }
+      `}</style>
+      <style jsx global>{`
+        .badge-secondary,
+        .btn-secondary,
+        .bg-secondary {
+          background-color: #c04545 !important;
+          border-color: #c04545 !important;
+        }
+      `}</style>
+      <style jsx global>{`
+        .badge-light,
+        .btn-light,
+        .bg-light {
+          background-color: #ff8080 !important;
+          border-color: #ff8080 !important;
+          color: #ffffff !important;
+        }
+      `}</style>
+      <style jsx global>{`
+        .badge-info,
+        .btn-info,
+        .bg-info {
+          background-color: #0a0a80 !important;
+          border-color: #0a0a80 !important;
+        }
+      `}</style>
+    </>
+  );
 }
