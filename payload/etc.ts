2c67083136a4cac6c880bd788cf24c63ab34006e
Zerohertz <ohg3417@gmail.com>
2023-11-06 22:15:59 +0900

    :memo: Update: BOAZ

--- payload/etc.ts
+++ payload/etc.ts
@@ -10,6 +10,12 @@ const etc: IEtc.Payload = {
       startedAt: '2023-02',
       endedAt: '2026-02',
     },
+    {
+      title: 'BOAZ 19기 수료',
+      subTitle: '국내 최초 빅데이터 동아리 (분석 부문)',
+      startedAt: '2022-07',
+      endedAt: '2023-07',
+    },
   ],
 };
 
