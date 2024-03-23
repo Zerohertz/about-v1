7d2ec0f44d7903198cd31bad03d40f79ce511130
Zerohertz <ohg3417@gmail.com>
2024-03-24 01:33:52 +0900

    :memo: Update: Introduce

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -7,7 +7,7 @@ const introduce: IIntroduce.Payload = {
   contents: [
     '안녕하세요, 2년차 Machine Learning Engineer 오효근 입니다.',
     '다룰 수 있는 기술은 Python, Docker, Kubernetes, Jenkins 등이 있습니다.',
-    'AI 기반의 제품을 개발하기위해 MLOps를 통해 자동화하고 협업이 가능한 환경을 구축하는 것을 선호합니다.',
+    'AI 기반 제품의 개발을 위해 지속적이며 유연한 협업이 가능한 MLOps 환경을 구축하는 것을 선호합니다.',
     '명료하지만 엄격한 코드를 개발하고 공유하며 협업하는 것에 행복을 느낍니다.',
   ],
   sign: 'Zerohertz',
